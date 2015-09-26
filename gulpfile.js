3fb8592bdc8280e70e49f3db1760a09b8d57bed8
Artur Grzegorzewicz <artur.grzegorzewicz@gmail.com>
2015-09-26 17:01:32 +0200

    Live Server - watching for changes.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,24 @@ var loadPlugins = require('gulp-load-plugins');
 
 var $ = loadPlugins();
 
-gulp.task('serve', function() {
+gulp.task('serve', function () {
 	var server = $.liveServer.static('app', 8080);
 	server.start();
+
+	gulp.watch(['app/**/*.css', 'app/**/*.html', 'app/**/*.js'], function (event) {
+		log('File: ' + event.path + ' has been changed. Reloading...');
+		server.notify.apply(server, [event]);
+	});
 });
+
+function log(msg) {
+	if (typeof (msg) === 'object') {
+		for (var item in msg) {
+			if (msg.hasOwnProperty(item)) {
+				$.util.log($.util.colors.blue(item + ': ' + msg[item]));
+			}
+		}
+	} else {
+		$.util.log($.util.colors.blue(msg));
+	}
+}
